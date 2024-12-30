443ec6e7927a7495bb2a01f1b28b10cd568a113b
TheAGamePlayer <79672176+TheAGamePlayer@users.noreply.github.com>
2024-12-29 17:25:47 -0800

    See 1.6.6.6R-LTS1 Changelog

--- src/main/java/dev/theagameplayer/puresuffering/mixin/SensorMixin.java
+++ src/main/java/dev/theagameplayer/puresuffering/mixin/SensorMixin.java
@@ -10,25 +10,24 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.sensing.Sensor;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
-import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
 
 @Mixin(Sensor.class)
 public final class SensorMixin {
-    //private static final TargetingConditions INVASION_ATTACK_TARGET_CONDITIONS = TargetingConditions.forCombat();
-    //private static final TargetingConditions INVASION_ATTACK_TARGET_CONDITIONS_IGNORE_INVISIBILITY_TESTING = TargetingConditions.forCombat().ignoreInvisibilityTesting();
+    private static final TargetingConditions INVASION_ATTACK_TARGET_CONDITIONS = TargetingConditions.forCombat();
+    private static final TargetingConditions INVASION_ATTACK_TARGET_CONDITIONS_IGNORE_INVISIBILITY_TESTING = TargetingConditions.forCombat().ignoreInvisibilityTesting();
 	private static final TargetingConditions INVASION_ATTACK_TARGET_CONDITIONS_IGNORE_LINE_OF_SIGHT = TargetingConditions.forCombat().ignoreLineOfSight();
 	private static final TargetingConditions INVASION_ATTACK_TARGET_CONDITIONS_IGNORE_INVISIBILITY_AND_LINE_OF_SIGHT = TargetingConditions.forCombat().ignoreLineOfSight().ignoreInvisibilityTesting();
 
-	/*@Inject(at = @At("HEAD"), method = "isEntityAttackable(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/LivingEntity;)Z", cancellable = true)
+	@Inject(at = @At("HEAD"), method = "isEntityAttackable(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/LivingEntity;)Z", cancellable = true)
 	private static final void isEntityAttackable(final LivingEntity pEntity, final LivingEntity pTarget, final CallbackInfoReturnable<Boolean> pCallback) {
-		if (pEntity.getPersistentData().contains(Invasion.INVASION_MOB) && pEntity instanceof Breeze)
+		if (pEntity.getPersistentData().contains(Invasion.INVASION_MOB))
 			pCallback.setReturnValue(pEntity.getBrain().isMemoryValue(MemoryModuleType.ATTACK_TARGET, pTarget) ? INVASION_ATTACK_TARGET_CONDITIONS_IGNORE_INVISIBILITY_TESTING.test(pEntity, pTarget) : INVASION_ATTACK_TARGET_CONDITIONS.test(pEntity, pTarget));
-	}*/
+	}
 	
 	//A temporary fix to an annoying problem with Piglins being unable to have hyper aggression applied?
 	@Inject(at = @At("HEAD"), method = "isEntityAttackableIgnoringLineOfSight(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/LivingEntity;)Z", cancellable = true)
 	private static final void isEntityAttackableIgnoringLineOfSight(final LivingEntity pEntity, final LivingEntity pTarget, final CallbackInfoReturnable<Boolean> pCallback) {
-		if (pEntity.getPersistentData().contains(Invasion.INVASION_MOB) && pEntity instanceof AbstractPiglin)
+		if (pEntity.getPersistentData().contains(Invasion.INVASION_MOB))
 			pCallback.setReturnValue(pEntity.getBrain().isMemoryValue(MemoryModuleType.ATTACK_TARGET, pTarget) ? INVASION_ATTACK_TARGET_CONDITIONS_IGNORE_INVISIBILITY_AND_LINE_OF_SIGHT.test(pEntity, pTarget) : INVASION_ATTACK_TARGET_CONDITIONS_IGNORE_LINE_OF_SIGHT.test(pEntity, pTarget));
 	}
 }
